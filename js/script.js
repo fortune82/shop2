78903fe0fecabf2f4857e1b749cef9b54c5b39fa
fortuna8.2 <fortuna8228@gmail.com>
2020-06-22 23:20:03 +0300

    создал 3 секции

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.headMain').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+    });
+});
